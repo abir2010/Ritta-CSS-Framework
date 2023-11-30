ef03356fa6f5d429c3515aa5957fc17f0af44c7b
abir2010 <kaiserabir222@gmail.com>
2023-11-30 18:59:51 +0600

    completed with a demo homepage

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const sass = require("gulp-sass")(require("sass"));
 // function for compileing our SASS into CSS
 function buildStyles() {
     return src("ritta/**/*.scss").pipe(sass()).pipe(dest("css")); // output in a css folder
+    // .pipe(purgecss({content:['*.html']}))
 }
 
 // watch function that will actively watch our source SASS file and then when we make changes to that file and save them it's going to automatically runs the buildStyles function for us so that everytime we make a change to our SASS file oyur CSS is going to be updated and processed automatically to stay in sync with it
